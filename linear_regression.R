28458e667b2dadbd93436ba79489e5b7e09c59e9
Arjun <arjun.conjecture@gmail.com>
2025-05-31 15:48:30 +0530

    r regression

--- linear_regression.R
+++ linear_regression.R
@@ -1,4 +1,6 @@
-data = read.csv('C:\\Users\\arjun\\OneDrive\\Desktop\\Random\\Math Club\\Sublime App\\lasso_data.dat')
+library(ggplot)
+
+data = read.csv('C:\\Users\\arjun\\OneDrive\\Desktop\\Random\\Math Club\\Sublime App\\r_regression.csv')
 summary(data)
 
-model <- lm(class ~ x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, data=data)
+model <- lm(y ~ x, data=data)
